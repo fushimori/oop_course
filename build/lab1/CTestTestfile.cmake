37bac04e2acd21d023ada291dcaeda063c4569d1
Mori <129299467+fushimori@users.noreply.github.com>
2023-09-16 14:22:05 +0300

    Delete build directory

--- build/lab1/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maksim/mai/course2/sem3/oop/lab1
-# Build directory: /home/maksim/mai/course2/sem3/oop/build/lab1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(counter_test "/home/maksim/mai/course2/sem3/oop/build/lab1/gtest")
-set_tests_properties(counter_test PROPERTIES  _BACKTRACE_TRIPLES "/home/maksim/mai/course2/sem3/oop/lab1/CMakeLists.txt;16;add_test;/home/maksim/mai/course2/sem3/oop/lab1/CMakeLists.txt;0;")
