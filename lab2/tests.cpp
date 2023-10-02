307f6d068456b2dad765f7d209cbea7e92718190
Maks <maksim19724@yandex.ru>
2023-10-02 23:42:44 +0300

    Add missing tests

--- lab2/tests.cpp
+++ lab2/tests.cpp
@@ -12,8 +12,7 @@ TEST(twelve_test, char_constructor){
     ASSERT_EQ(tw.get_size(), 100);
     unsigned char* arr = tw.get_arr();
     for(size_t i = 0; i < tw.get_size(); i++)
-        ASSERT_EQ(arr[i], '0');
-    
+        ASSERT_EQ(arr[i], '0'); 
 }
 
 TEST(twelve_test, init_list_constructor){
@@ -40,19 +39,47 @@ TEST(twelve_test, string_constructor){
     
 }
 
-TEST(twelve_test, exp_char_constructor){
+TEST(twelve_test, exp_char_constructor_1){
     ASSERT_THROW(Twelve tw(3,'-'), std::invalid_argument);
 }
-TEST(twelve_test, exp_init_list_constructor){
+TEST(twelve_test, exp_char_constructor_2){
+    ASSERT_THROW(Twelve tw(3,'C'), std::invalid_argument);
+}
+TEST(twelve_test, exp_char_constructor_3){
+    char s;
+    ASSERT_THROW(Twelve tw(3,s), std::invalid_argument);
+}
+
+TEST(twelve_test, exp_init_list_constructor_1){
     ASSERT_THROW(Twelve tw{"12-"}, std::invalid_argument);
 }
-TEST(twelve_test, exp_string_constructor){
-    std::string str{"WDZXCAB0101"};
+TEST(twelve_test, exp_init_list_constructor_2){
+    ASSERT_THROW(Twelve tw{"12С"}, std::invalid_argument);
+}
+TEST(twelve_test, exp_init_list_constructor_3){
+    ASSERT_THROW(Twelve tw{""}, std::invalid_argument);
+}
+
+TEST(twelve_test, exp_string_constructor_1){
+    std::string str{"10D"};
+    ASSERT_THROW(Twelve tw(str), std::invalid_argument);
+}
+TEST(twelve_test, exp_string_constructor_2){
+    std::string str{"23+1"};
+    ASSERT_THROW(Twelve tw(str), std::invalid_argument);
+}
+TEST(twelve_test, exp_string_constructor_3){
+    std::string str{""};
     ASSERT_THROW(Twelve tw(str), std::invalid_argument);
 }
 
 TEST(twelve_test, copy){
     Twelve tw1("ABABA");
+    Twelve tw2(tw1);
+    ASSERT_TRUE(tw1.equals(tw2));
+}
+TEST(twelve_test, copy_1){
+    Twelve tw1;
     Twelve tw2(tw1);
     ASSERT_TRUE(tw1.equals(tw2));
 }
@@ -68,12 +95,30 @@ TEST(twelve_test, move){
     for(size_t i = 0; i < 4; i++)
         ASSERT_EQ(arr[i], str[4 - i]);
 }
+TEST(twelve_test, move_1){
+    Twelve tw1;
+    Twelve tw2(std::move(tw1));
+    ASSERT_EQ(tw1.get_size(), 0);
+    ASSERT_EQ(tw1.get_arr(), nullptr);
+    ASSERT_EQ(tw2.get_size(), 0);
+    ASSERT_EQ(tw2.get_arr(), nullptr);
+}
 
 TEST(twelve_test, equals){
     std::string str{"0000001"};
     Twelve tw1{str};
     Twelve tw2{"001"};
     ASSERT_TRUE(tw1.equals(tw2));
+}
+TEST(twelve_test, equals_1){
+    Twelve tw1;
+    Twelve tw2;
+    ASSERT_TRUE(tw1.equals(tw2));
+}
+TEST(twelve_test, equals_2){
+    Twelve tw1;
+    Twelve tw2("123");
+    ASSERT_FALSE(tw1.equals(tw2));
 }
 
 TEST(twelve_test, greater){
@@ -82,11 +127,35 @@ TEST(twelve_test, greater){
     Twelve tw2{"ABA"};
     ASSERT_TRUE(tw1.greater(tw2));
 }
+TEST(twelve_test, greater_1){
+    std::string str{"ABA"};
+    Twelve tw1{str};
+    Twelve tw2{"ABB"};
+    ASSERT_FALSE(tw1.greater(tw2));
+}
+TEST(twelve_test, greater_2){
+    std::string str{"AAA"};
+    Twelve tw1{str};
+    Twelve tw2{"AAA"};
+    ASSERT_FALSE(tw1.greater(tw2));
+}
 
 TEST(twelve_test, less){
     std::string str{"ABB"};
     Twelve tw1{str};
     Twelve tw2{"ABA"};
+    ASSERT_FALSE(tw1.less(tw2));
+}
+TEST(twelve_test, less_1){
+    std::string str{"ABA"};
+    Twelve tw1{str};
+    Twelve tw2{"ABB"};
+    ASSERT_TRUE(tw1.less(tw2));
+}
+TEST(twelve_test, less_2){
+    std::string str{"AAA"};
+    Twelve tw1{str};
+    Twelve tw2{"AAA"};
     ASSERT_FALSE(tw1.less(tw2));
 }
 
@@ -120,6 +189,28 @@ TEST(twelve_test, add_3){
     for(size_t i = 0; i < 5; i++)
         ASSERT_EQ(arr[i], rez[4 - i]);
 }
+TEST(twelve_test, add_4){
+    std::string str{"1111"};
+    Twelve tw1;
+    Twelve tw2(str);
+    ASSERT_TRUE(tw2.equals(tw1.add(tw2)));
+}
+TEST(twelve_test, add_5){
+    std::string str{"1111"};
+    Twelve tw1(str);
+    Twelve tw2;
+    ASSERT_TRUE(tw1.equals(tw1.add(tw2)));
+}
+TEST(twelve_test, add_6){
+    Twelve tw1;
+    Twelve tw2;
+    ASSERT_TRUE(tw1.equals(tw1.add(tw2)));
+}
+TEST(twelve_test, add_7){
+    Twelve tw1("0");
+    Twelve tw2("0");
+    ASSERT_TRUE(tw1.equals(tw1.add(tw2)));
+}
 
 TEST(twelve_test, subtract_1){
     std::string str{"BBB"};
@@ -136,25 +227,75 @@ TEST(twelve_test, subtract_2){
     Twelve tw1{str};
     Twelve tw2{"BBB"};
     Twelve tw3 = tw1.subtract(tw2);
-    std::string rez{"0"};
     unsigned char* arr = tw3.get_arr();
-    for(size_t i = 0; i < 1; i++)
-        ASSERT_EQ(arr[i], rez[i]);
+    ASSERT_EQ(arr[0], '0');
 }
-TEST(twelve_test, subtract_exp){
+TEST(twelve_test, subtract_3){
+    Twelve tw1{"AAA"};
+    Twelve tw2;
+    ASSERT_TRUE(tw1.equals(tw1.subtract(tw2)));
+}
+TEST(twelve_test, subtract_4){
+    Twelve tw1;
+    Twelve tw2;
+    Twelve tw3 = tw1.subtract(tw2);
+    unsigned char* arr = tw3.get_arr();
+    ASSERT_EQ(arr[0],'0');
+}
+TEST(twelve_test, subtract_5){
+    Twelve tw1("0");
+    Twelve tw2("0");
+    Twelve tw3 = tw1.subtract(tw2);
+    unsigned char* arr = tw3.get_arr();
+    ASSERT_EQ(arr[0],'0');
+}
+
+TEST(twelve_test, subtract_exp_1){
     std::string str{"101"};
     Twelve tw1{str};
     Twelve tw2{"BBB"};
     ASSERT_THROW(tw1.subtract(tw2), std::logic_error);
 }
+TEST(twelve_test, subtract_exp_2){
+    Twelve tw1;
+    Twelve tw2{"BBB"};
+    ASSERT_THROW(tw1.subtract(tw2), std::logic_error);
+}
 
 TEST(twelve_test, print) {
-    Twelve tw{'1', '2', 'A', 'B'};
+    Twelve tw{'0','1', '2', 'A', 'B'};
     std::ostringstream os;
 
     tw.print(os);
 
     std::string expectedOutput = "12AB";
+    EXPECT_EQ(os.str(), expectedOutput);
+}
+TEST(twelve_test, print_1) {
+    Twelve tw(100);
+    std::ostringstream os;
+
+    tw.print(os);
+
+    std::string expectedOutput = "0";
+    EXPECT_EQ(os.str(), expectedOutput);
+}
+TEST(twelve_test, print_2) {
+    Twelve tw{"00012345"};
+    std::ostringstream os;
+
+    tw.print(os);
+
+    std::string expectedOutput = "12345";
+    EXPECT_EQ(os.str(), expectedOutput);
+}
+TEST(twelve_test, print_3) {
+    Twelve tw;
+    std::ostringstream os;
+
+    tw.print(os);
+
+    std::string expectedOutput = "";
     EXPECT_EQ(os.str(), expectedOutput);
 }
 
